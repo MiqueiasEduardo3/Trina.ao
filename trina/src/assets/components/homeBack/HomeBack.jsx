3ab6804866aeab88dfc24cf6b9f7271ed6ae59ea
Eduardvianga31 <136762292+Eduardvianga31@users.noreply.github.com>
2025-02-12 21:41:10 +0100

    Homeback.jsx
    Update slide images in the components
    with swiper

--- trina/src/assets/components/homeBack/HomeBack.jsx
+++ trina/src/assets/components/homeBack/HomeBack.jsx
@@ -1,33 +1,25 @@
-import './HomeBack.css'
+import "./HomeBack.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay, EffectFade } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/effect-fade";
-import { hero2 } from './../../../img/hero2.png';
-import { hero3 } from './../../../img/hero3.png';
-import { bg } from './../../../img/bg.jpg';
 
+// Importação correta das imagens
+import hero2 from "./../../../img/hero2.png";
+import hero3 from "./../../../img/hero3.png";
+import bg from "./../../../img/bg.jpg";
 
-const HomeBack = () =>{
-    const slides = [
-        {
-          id: 1,
-          image: {hero2},
-        },
-        {
-          id: 2,
-          image: {hero3},
-        },
-        {
-          id: 3,
-          image: {bg},
-        },
-      ];
-    
-    return(
-        <div className="w-full h-[500px] lg:h-[700px]">
+const HomeBack = () => {
+  const slides = [
+    { id: 1, image: hero2 },
+    { id: 2, image: hero3 },
+    { id: 3, image: bg },
+  ];
+
+  return (
+    <div className="w-full h-[500px] lg:h-[700px]">
       <Swiper
         modules={[Navigation, Pagination, Autoplay, EffectFade]}
         effect="fade"
@@ -40,12 +32,9 @@ const HomeBack = () =>{
         {slides.map((slide) => (
           <SwiperSlide key={slide.id}>
             <div
-              className="w-full h-full flex items-center justify-center bg-cover bg-center"
+              className="w-full h-full bg-cover bg-center"
               style={{ backgroundImage: `url(${slide.image})` }}
-            >
-              <div className="bg-black/50 text-white p-8 rounded-lg text-center">
-              </div>
-            </div>
+            />
           </SwiperSlide>
         ))}
       </Swiper>
