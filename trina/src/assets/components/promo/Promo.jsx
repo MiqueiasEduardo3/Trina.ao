6d47fc6f98fbc6eb585c051a7c7e977e27abd6f0
Eduardvianga31 <136762292+Eduardvianga31@users.noreply.github.com>
2025-02-13 23:47:57 +0100

    Update-Promo.jsx
    I remove de font-estrabold to font-semibold in title PROMOÇÃO DA PURPLE JANUARY

--- trina/src/assets/components/promo/Promo.jsx
+++ trina/src/assets/components/promo/Promo.jsx
@@ -82,7 +82,7 @@ const Promo = () =>{
         
         <div className='h-full  items-center flex bg-gray-100 flex-col justify-center '>
             <section className='w-full h-10 bg-red-800 p-7 flex items-center  justify-between'> 
-                 <h2 className="font-extrabold text-white text-xs md:text-sm  -ml-4 md:ml-0 lg:md-0 lg:text-lg">
+                 <h2 className="font-semibold text-white text-xs md:text-sm -ml-4 md:ml-0 lg:md-0 lg:text-lg">
                   <FontAwesomeIcon className="w-10 lg:text-lg md:text-lg" icon={faClock} />PROMOÇÃO DA PURPLE JANUARY</h2> 
 
                   <div className="flex items-center gap-14 lg:gap-6 w-1/6 lg:w-2/5 md:w-6/12"> 
